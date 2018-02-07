967f296eac3e52cdc740262a9d0dda357ad360ec
David Wu <david10608@gmail.com>
2018-02-07 23:50:31 +0100

    color styling

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,5 +1,5 @@
 const colors = {
-  background: '#1F0808',
+  background: '#00cc99',
   clear: 'rgba(0,0,0,0)',
   facebook: '#3b5998',
   transparent: 'rgba(0,0,0,0)',
@@ -7,7 +7,7 @@ const colors = {
   steel: '#CCCCCC',
   error: 'rgba(200, 0, 0, 0.8)',
   ricePaper: 'rgba(255,255,255, 0.75)',
-  frost: '#D8D8D8',
+  frost: '#cdf5eb',
   cloud: 'rgba(200,200,200, 0.35)',
   windowTint: 'rgba(0, 0, 0, 0.4)',
   panther: '#161616',
@@ -21,7 +21,7 @@ const colors = {
   eggplant: '#251a34',
   border: '#483F53',
   banner: '#5F3E63',
-  text: '#E0D7E5'
+  text: '#000000'
 }
 
 export default colors
